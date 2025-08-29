5587c03007243f969c2ac3863f34a3379929114a
Zdeněk Vydra <vydra@cshub.cz>
2025-08-29 13:20:00 +0200

    Update terraform.tf, fix mgmt_user

--- terraform.tf
+++ terraform.tf
@@ -1,60 +1,50 @@
 terraform {
-  backend "http" {
-  }
   required_providers {
-    kypo = {
-      source = "vydrazde/kypo"
+    crczp = {
+      source = "cyberrangecz/crczp"
     }
     openstack = {
       source = "terraform-provider-openstack/openstack"
     }
-    gitlab = {
-      source = "gitlabhq/gitlab"
-    }
+  }
+
+  backend "s3" {
+    endpoint                    = "gm7ve.upcloudobjects.com"
+    bucket                      = "cshub-states"
+    encrypt                     = true
+    force_path_style            = true
+    key                         = var.NAME
+    region                      = "europe-2"
+    skip_credentials_validation = true
+    skip_region_validation      = true
+    workspace_key_prefix        = "images-ci"
   }
 }
 
-provider "kypo" {
-  endpoint = var.KYPO_ENDPOINT
+provider "crczp" {
 }
 
 provider "openstack" {
-  auth_url = "https://identity.cloud.muni.cz/v3"
-  region   = "brno1"
 }
 
-provider "gitlab" {
-  base_url = "https://${var.CI_SERVER_HOST}/api/v4"
-  token    = var.ACCESS_TOKEN
-}
-
-variable "KYPO_ENDPOINT" {}
-variable "CI_PROJECT_ID" {}
-variable "CI_PROJECT_URL" {}
-variable "CI_COMMIT_SHORT_SHA" {}
-variable "CI_SERVER_HOST" {}
-variable "CI_PROJECT_PATH" {}
+variable "PROJECT_URL" {}
+variable "REV" {}
 variable "NAME" {}
 variable "TYPE" {}
 variable "DISTRO" {}
 variable "GUI_ACCESS" {}
-variable "ACCESS_TOKEN" {
-  sensitive = true
+variable "IMAGE_LOCAL_PATH" {
+  type    = string
+  default = null
 }
 
 module "topology" {
-  source               = "gitlab.ics.muni.cz/muni-kypo-images/image-testing-topology/kypo"
-  kypo_endpoint        = var.KYPO_ENDPOINT
-  project_url          = "${var.CI_SERVER_HOST}/${var.CI_PROJECT_PATH}"
-  project_access_token = var.ACCESS_TOKEN
-  project_id           = var.CI_PROJECT_ID
-  rev                  = var.CI_COMMIT_SHORT_SHA
-  image_name           = var.NAME
-  os_type              = var.TYPE
-  os_distro            = var.DISTRO
-  gui_access           = var.GUI_ACCESS
-}
-
-output "pool_url" {
-  value = module.topology.pool_url
+  source           = "git::https://github.com/cyberrangecz/terraform-crczp-image-testing-topology.git"
+  project_url      = var.PROJECT_URL
+  rev              = var.REV
+  image_name       = var.NAME
+  os_type          = var.TYPE
+  os_distro        = var.DISTRO
+  gui_access       = var.GUI_ACCESS
+  image_local_path = var.IMAGE_LOCAL_PATH
 }
